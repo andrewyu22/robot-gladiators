db446111e2d12871073f61d760f0bfa9e5339795
Andrew Yu <andrewyu209@gmail.com>
2021-10-13 12:17:02 -0400

    Fixed player name bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,59 +1,12 @@
-  
-  // player information
-  var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
-    health: 100,
-    attack: 10,
-    money: 10,
-    reset: function() {
-      this.health = 100;
-      this.money = 10;
-      this.attack = 10;
-    },
-    refillHealth: function() {
-      if (this.money >= 7) {
-        window.alert("Refilling player's health by 20 for 7 dollars.");
-        this.health += 20;
-        this.money -= 7;
-      } 
-      else {
-        window.alert("You don't have enough money!");
-      }
-    },
-    upgradeAttack: function() {
-      if (this.money >= 7) {
-        window.alert("Upgrading player's attack by 6 for 7 dollars.");
-        this.attack += 6;
-        this.money -= 7;
-      } 
-      else {
-        window.alert("You don't have enough money!");
-      }
-    }
-  };
-  
-  // enemy information
-  var enemyInfo = [
-    {
-      name: 'Roborto',
-      attack: randomNumber(10, 14)
-    },
-    {
-      name: 'Amy Android',
-      attack: randomNumber(10, 14)
-    },
-    {
-      name: 'Robo Trumble',
-      attack: randomNumber(10, 14)
-    }
-  ];
+/* GAME FUNCTIONS */
+
 // function to generate a random numeric value
 var randomNumber = function(min, max) {
     var value = Math.floor(Math.random() * (max - min + 1) + min);
   
     return value;
   };
-  
+
   // fight function (now with parameter for enemy's object holding name, health, and attack values)
   var fight = function(enemy) {
     while (playerInfo.health > 0 && enemy.health > 0) {
@@ -127,7 +80,6 @@ var randomNumber = function(min, max) {
       if (playerInfo.health > 0) {
         // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
         window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
-  
         // pick new enemy to fight based on the index of the enemyInfo array
         var pickedEnemyObj = enemyInfo[i];
   
@@ -215,7 +167,64 @@ var randomNumber = function(min, max) {
   /* END GAME FUNCTIONS */
   
   /* GAME INFORMATION / VARIABLES */
-
+  // function to set name
+  var getPlayerName = function() {
+    var name = "";
+    while (name === "" || name === null)
+    {
+      name = prompt("What is your robot's name?");
+    }
+    console.log("Your robot's name is " + name);
+    return name;
+  };
+  // player information
+  var playerInfo = {
+    name: getPlayerName(),
+    health: 100,
+    attack: 10,
+    money: 10,
+    reset: function() {
+      this.health = 100;
+      this.money = 10;
+      this.attack = 10;
+    },
+    refillHealth: function() {
+      if (this.money >= 7) {
+        window.alert("Refilling player's health by 20 for 7 dollars.");
+        this.health += 20;
+        this.money -= 7;
+      } 
+      else {
+        window.alert("You don't have enough money!");
+      }
+    },
+    upgradeAttack: function() {
+      if (this.money >= 7) {
+        window.alert("Upgrading player's attack by 6 for 7 dollars.");
+        this.attack += 6;
+        this.money -= 7;
+      } 
+      else {
+        window.alert("You don't have enough money!");
+      }
+    }
+  };
+  
+  // enemy information
+  var enemyInfo = [
+    {
+      name: 'Roborto',
+      attack: randomNumber(10, 14)
+    },
+    {
+      name: 'Amy Android',
+      attack: randomNumber(10, 14)
+    },
+    {
+      name: 'Robo Trumble',
+      attack: randomNumber(10, 14)
+    }
+  ];
   
   console.log(enemyInfo);
   console.log(enemyInfo[0]);
